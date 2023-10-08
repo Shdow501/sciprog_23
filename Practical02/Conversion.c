3428cae0f5c03a4e56b812c1b2e67df9408818cc
Suman <suman.bhattacharjee501@gmail.com>
2023-10-08 20:02:30 +0100

    Added string reversal

--- Practical02/Conversion.c
+++ Practical02/Conversion.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 #include <math.h>
+#include <string.h>
 
 int main(void) {
 
 /* Declare variables */
-   int i,inum,tmp,numdigits;
+   int i,inum,tmp,numdigits,length,mid,j;
    float fnum;
-   char binnum[60];
+   char binnum[60],aux;
 
 /* Intialise 4-byte integer */
    inum = 33554431;
@@ -24,7 +25,16 @@ int main(void) {
 
 /* Terminate the string */
    binnum[i] = '\0';
-       
+
+/* Reversing the string */
+   length = strlen(binnum);
+   mid = length/2;
+   for (i=0;i<mid;i++){
+    j = length-i-1;
+    aux = binnum[i];
+    binnum[i] = binnum[j];      
+    binnum[j] = aux;
+   }
 
 /* Complete the expression */
 numdigits = ceil(logf(inum)/logf(2) );
